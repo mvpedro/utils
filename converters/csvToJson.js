26462545deda49a76be2efbbc543871e0c1664f8
Pedro Vieira <mvpedro.cc@gmail.com>
2023-11-26 10:49:53 -0300

    Adding saving to file

--- converters/csvToJson.js
+++ converters/csvToJson.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-function convertCsvToJson(csvFilePath, fieldMappings) {
+function convertCsvToJson(csvFilePath, fieldMappings, jsonOutputPath) {
   return new Promise((resolve, reject) => {
     const results = [];
 
@@ -15,18 +15,25 @@ function convertCsvToJson(csvFilePath, fieldMappings) {
         results.push(jsonData);
       })
       .on('end', () => {
-        resolve(results);
+        fs.writeFile(jsonOutputPath, JSON.stringify(results, null, 2), (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(`JSON data saved to ${jsonOutputPath}`);
+          }
+        });
       })
       .on('error', reject);
   });
 }
 
-const csvFilePath = 'path/to/your/csvfile.csv';
+const csvFilePath = 'path/to/your/csvfile.csv'; 
 const fieldMappings = {
   jsonFieldName1: 'CSVColumnName1',
   jsonFieldName2: 'CSVColumnName2',
 };
+const jsonOutputPath = 'output.json'; 
 
-convertCsvToJson(csvFilePath, fieldMappings)
-  .then(jsonData => console.log(jsonData))
+convertCsvToJson(csvFilePath, fieldMappings, jsonOutputPath)
+  .then(message => console.log(message))
   .catch(error => console.error('Error:', error));
