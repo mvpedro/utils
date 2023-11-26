856b5e40d98d2165f9d1ca4e0a22e48282d88821
Pedro Vieira <mvpedro.cc@gmail.com>
2023-11-26 11:09:13 -0300

    Formatting

--- converters/csvToJson.js
+++ converters/csvToJson.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const csv = require('csv-parser');
+const fs = require("fs");
+const csv = require("csv-parser");
 
 function convertCsvToJson(csvFilePath, fieldMappings, jsonOutputPath) {
   return new Promise((resolve, reject) => {
@@ -7,36 +7,40 @@ function convertCsvToJson(csvFilePath, fieldMappings, jsonOutputPath) {
 
     fs.createReadStream(csvFilePath)
       .pipe(csv())
-      .on('data', (data) => {
+      .on("data", (data) => {
         const jsonData = {};
         for (const [jsonField, csvColumn] of Object.entries(fieldMappings)) {
           jsonData[jsonField] = data[csvColumn];
         }
         results.push(jsonData);
       })
-      .on('end', () => {
-        fs.writeFile(jsonOutputPath, JSON.stringify(results, null, 2), (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(`JSON data saved to ${jsonOutputPath}`);
+      .on("end", () => {
+        fs.writeFile(
+          jsonOutputPath,
+          JSON.stringify(results, null, 2),
+          (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(`JSON data saved to ${jsonOutputPath}`);
+            }
           }
-        });
+        );
       })
-      .on('error', reject);
+      .on("error", reject);
   });
 }
 
-const csvFilePath = '../inputs/csv_alumni_db.csv'; 
+const csvFilePath = "../inputs/csv_alumni_db.csv";
 const fieldMappings = {
-    nome: 'Nome completo',
-  email: 'e-mail',
-  dataDeIngresso: 'Ano Ingresso',
-  dataDeFormatura: 'Ano Formatura',
-  empresaAtual: 'Qual a empresa/organização que está trabalhando agora?',  
+  nome: "Nome completo",
+  email: "e-mail",
+  dataDeIngresso: "Ano Ingresso",
+  dataDeFormatura: "Ano Formatura",
+  empresaAtual: "Qual a empresa/organização que está trabalhando agora?",
 };
-const jsonOutputPath = '../outputs/csvToJson.json'; 
+const jsonOutputPath = "../outputs/csvToJson.json";
 
 convertCsvToJson(csvFilePath, fieldMappings, jsonOutputPath)
-  .then(message => console.log(message))
-  .catch(error => console.error('Error:', error));
+  .then((message) => console.log(message))
+  .catch((error) => console.error("Error:", error));
